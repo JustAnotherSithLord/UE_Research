7b7871004cb8076d439a3a94d93c3d0bff47528e
Michael <163198007+JustAnotherSithLord@users.noreply.github.com>
2024-03-12 15:18:49 -0500

    Update Batman_Arkham_Knight.c

--- Batman_Arkham_Knight.c
+++ Batman_Arkham_Knight.c
@@ -1,1 +1,5 @@
-
+Ghost                                    ( FunctionEngine.CheatManager.Ghost )
+DebugAI                                  ( FunctionEngine.CheatManager.DebugAI )
+DebugPause                               ( FunctionEngine.CheatManager.DebugPause )
+ListDynamicActors                        ( FunctionEngine.CheatManager.ListDynamicActors )
+FreezeFrame                              ( FunctionEngine.CheatManager.FreezeFrame )
