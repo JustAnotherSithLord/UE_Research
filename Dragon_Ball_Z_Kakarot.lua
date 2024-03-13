9b7aa8ea409065851e7d5f9cc6ac405eba85b456
Michael <163198007+JustAnotherSithLord@users.noreply.github.com>
2024-03-12 19:04:14 -0500

    Update Dragon_Ball_Z_Kakarot.lua

--- Dragon_Ball_Z_Kakarot.lua
+++ Dragon_Ball_Z_Kakarot.lua
@@ -1,1 +1,5 @@
-
+[ Index:00000B630] (Size:0x000B8) [UObject:000001E199B7D000] Function  /Script/Engine.CheatManager:BugIt
+[ Index:00000B631] (Size:0x00070) [UObject:000001E199C22460] StrProperty  /Script/Engine.CheatManager:BugIt.ScreenShotDescription
+[ Index:00000B632] (Size:0x000B8) [UObject:000001E199B7CF40] Function  /Script/Engine.CheatManager:BugItGo
+[ Index:00000B633] (Size:0x00070) [UObject:000001E199C223F0] FloatProperty  /Script/Engine.CheatManager:BugItGo.Roll
+[ Index:00000B634] (Size:0x00070) [UObject:000001E199C22380] FloatProperty  /Script/Engine.CheatManager:BugItGo.Yaw
