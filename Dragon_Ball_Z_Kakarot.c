e49843ba2d63a4384b8bc704f1d8cb7d10a5c035
Michael <163198007+JustAnotherSithLord@users.noreply.github.com>
2024-03-12 19:10:23 -0500

    Update Dragon_Ball_Z_Kakarot.c

--- Dragon_Ball_Z_Kakarot.c
+++ Dragon_Ball_Z_Kakarot.c
@@ -14,3 +14,29 @@ StructProperty  /Script/Engine.CheatManager:BugItStringCreator.ViewRotation	~	Sc
 StructProperty  /Script/Engine.CheatManager:BugItStringCreator.ViewLocation	~	ScriptStruct  /Script/CoreUObject.Vector
 Function  /Script/Engine.CheatManager:ChangeSize
 FloatProperty  /Script/Engine.CheatManager:ChangeSize.F
+Function  /Script/Engine.CheatManager:CheatScript
+StrProperty  /Script/Engine.CheatManager:CheatScript.ScriptName
+Function  /Script/Engine.CheatManager:DamageTarget
+FloatProperty  /Script/Engine.CheatManager:DamageTarget.DamageAmount
+Function  /Script/Engine.CheatManager:DebugCapsuleSweep
+Function  /Script/Engine.CheatManager:DebugCapsuleSweepCapture
+Function  /Script/Engine.CheatManager:DebugCapsuleSweepChannel
+ByteProperty  /Script/Engine.CheatManager:DebugCapsuleSweepChannel.Channel	~	Enum  /Script/Engine.ECollisionChannel
+Function  /Script/Engine.CheatManager:DebugCapsuleSweepClear
+Function  /Script/Engine.CheatManager:DebugCapsuleSweepComplex
+BoolProperty  /Script/Engine.CheatManager:DebugCapsuleSweepComplex.bTraceComplex
+Function  /Script/Engine.CheatManager:DebugCapsuleSweepPawn
+Function  /Script/Engine.CheatManager:DebugCapsuleSweepSize
+FloatProperty  /Script/Engine.CheatManager:DebugCapsuleSweepSize.Radius
+FloatProperty  /Script/Engine.CheatManager:DebugCapsuleSweepSize.HalfHeight
+Function  /Script/Engine.CheatManager:DestroyAll
+ClassProperty  /Script/Engine.CheatManager:DestroyAll.aClass	~	Class  /Script/CoreUObject.Class
+Function  /Script/Engine.CheatManager:DestroyAllPawnsExceptTarget
+Function  /Script/Engine.CheatManager:DestroyPawns
+ClassProperty  /Script/Engine.CheatManager:DestroyPawns.aClass	~	Class  /Script/CoreUObject.Class
+Function  /Script/Engine.CheatManager:DestroyServerStatReplicator
+Function  /Script/Engine.CheatManager:DestroyTarget
+Function  /Script/Engine.CheatManager:DisableDebugCamera
+Function  /Script/Engine.CheatManager:DumpChatState
+Function  /Script/Engine.CheatManager:DumpOnlineSessionState
+Function  /Script/Engine.CheatManager:DumpPartyState
