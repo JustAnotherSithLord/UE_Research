555b8599075347a659b58926d042483f5fcf3829
Michael <163198007+JustAnotherSithLord@users.noreply.github.com>
2024-03-15 00:56:15 -0500

    Update Dragon_Ball_Z_Kakarot.c

--- Dragon_Ball_Z_Kakarot.c
+++ Dragon_Ball_Z_Kakarot.c
@@ -40,3 +40,29 @@ Function  /Script/Engine.CheatManager:DisableDebugCamera
 Function  /Script/Engine.CheatManager:DumpChatState
 Function  /Script/Engine.CheatManager:DumpOnlineSessionState
 Function  /Script/Engine.CheatManager:DumpPartyState
+[ Index:00000B65A] (Size:0x000B8) [UObject:000001E199B7F100] Function  /Script/Engine.CheatManager:DumpVoiceMutingState
+[ Index:00000B65B] (Size:0x000B8) [UObject:000001E199B7F040] Function  /Script/Engine.CheatManager:EnableDebugCamera
+[ Index:00000B65C] (Size:0x000B8) [UObject:000001E199B7EF80] Function  /Script/Engine.CheatManager:FlushLog
+[ Index:00000B65D] (Size:0x000B8) [UObject:000001E199B7EEC0] Function  /Script/Engine.CheatManager:Fly
+[ Index:00000B65E] (Size:0x000B8) [UObject:000001E199B7EE00] Function  /Script/Engine.CheatManager:FreezeFrame
+[ Index:00000B65F] (Size:0x00070) [UObject:000001E199C21E40] FloatProperty  /Script/Engine.CheatManager:FreezeFrame.Delay
+[ Index:00000B660] (Size:0x000B8) [UObject:000001E199B7ED40] Function  /Script/Engine.CheatManager:Ghost
+[ Index:00000B661] (Size:0x000B8) [UObject:000001E199B7EC80] Function  /Script/Engine.CheatManager:God
+[ Index:00000B662] (Size:0x000B8) [UObject:000001E199B7EBC0] Function  /Script/Engine.CheatManager:InvertMouse
+[ Index:00000B663] (Size:0x000B8) [UObject:000001E199B7EB00] Function  /Script/Engine.CheatManager:LogLoc
+[ Index:00000B664] (Size:0x000B8) [UObject:000001E199B7EA40] Function  /Script/Engine.CheatManager:NuccDynDrawCollision
+[ Index:00000B665] (Size:0x00070) [UObject:000001E199C21DD0] StrProperty  /Script/Engine.CheatManager:NuccDynDrawCollision.Command
+[ Index:00000B666] (Size:0x000B8) [UObject:000001E199B7E980] Function  /Script/Engine.CheatManager:OnlyLoadLevel
+[ Index:00000B667] (Size:0x00070) [UObject:000001E199C21D60] NameProperty  /Script/Engine.CheatManager:OnlyLoadLevel.PackageName
+[ Index:00000B668] (Size:0x000B8) [UObject:000001E199B7E8C0] Function  /Script/Engine.CheatManager:PlayersOnly
+[ Index:00000B669] (Size:0x000B8) [UObject:000001E199B7E800] Function  /Script/Engine.CheatManager:ReceiveEndPlay
+[ Index:00000B66A] (Size:0x000B8) [UObject:000001E199B7E740] Function  /Script/Engine.CheatManager:ReceiveInitCheatManager
+[ Index:00000B66B] (Size:0x000B8) [UObject:000001E199B7E680] Function  /Script/Engine.CheatManager:ServerToggleAILogging
+[ Index:00000B66C] (Size:0x000B8) [UObject:000001E199B7E5C0] Function  /Script/Engine.CheatManager:SetMouseSensitivityToDefault
+[ Index:00000B66D] (Size:0x000B8) [UObject:000001E199B7E500] Function  /Script/Engine.CheatManager:SetWorldOrigin
+[ Index:00000B66E] (Size:0x000B8) [UObject:000001E199B7E440] Function  /Script/Engine.CheatManager:Slomo
+[ Index:00000B66F] (Size:0x00070) [UObject:000001E199C21CF0] FloatProperty  /Script/Engine.CheatManager:Slomo.NewTimeDilation
+[ Index:00000B670] (Size:0x000B8) [UObject:000001E199B7E380] Function  /Script/Engine.CheatManager:SpawnServerStatReplicator
+[ Index:00000B671] (Size:0x000B8) [UObject:000001E199B7E2C0] Function  /Script/Engine.CheatManager:StreamLevelIn
+[ Index:00000B672] (Size:0x00070) [UObject:000001E199C23960] NameProperty  /Script/Engine.CheatManager:StreamLevelIn.PackageName
+[ Index:00000B673] (Size:0x000B8) [UObject:000001E199B7E200] Function  /Script/Engine.CheatManager:StreamLevelOut
