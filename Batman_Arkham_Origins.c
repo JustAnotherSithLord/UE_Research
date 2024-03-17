daba70cd02c6107e20e3b702362c6fee9c476901
Michael <163198007+JustAnotherSithLord@users.noreply.github.com>
2024-03-17 00:17:57 -0500

    Update Batman_Arkham_Origins.c

--- Batman_Arkham_Origins.c
+++ Batman_Arkham_Origins.c
@@ -1,3 +1,9 @@
 // Batman Arkham Origins
 
 // Credit to Sunbeam for the amazing table
+
+ActivateAllGadgets ( Function BmGame.RCheatManager.ActivateAllGadgets )
+AddScreenShake ( Function BmGame.RCheatManager.AddScreenShake )
+AddXP ( Function BmGame.RCheatManager.AddXP )
+AllAmmo ( Function Engine.CheatManager.AllAmmo )
+AllWeapons ( Function Engine.CheatManager.AllWeapons )
